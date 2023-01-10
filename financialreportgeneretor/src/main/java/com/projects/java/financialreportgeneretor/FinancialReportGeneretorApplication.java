fda74d1ad9e9f86d98638dc4645adf0bd78f7c85
Bernardo Zacaron <bernardo.zacaron.profissional@gmail.com>
2023-01-10 15:42:36 -0300

    Blank project

---
+++ financialreportgeneretor/src/main/java/com/projects/java/financialreportgeneretor/FinancialReportGeneretorApplication.java
@@ -0,0 +1,13 @@
+package com.projects.java.financialreportgeneretor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialReportGeneretorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialReportGeneretorApplication.class, args);
+	}
+
+}
---
+++ financialreportgeneretor/src/test/java/com/projects/java/financialreportgeneretor/FinancialReportGeneretorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.java.financialreportgeneretor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialReportGeneretorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
